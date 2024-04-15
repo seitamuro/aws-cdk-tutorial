1b8615ba1d7e2cd589c13dc02e6e8ce15a0a0c4c
seitamuro <seitamuro0210@gmail.com>
2024-04-15 23:17:06 +0900

    add s3

---
+++ lib/aws-cdk-tutorial-stack.ts
@@ -0,0 +1,13 @@
+import * as cdk from "aws-cdk-lib";
+import { aws_s3 as s3 } from "aws-cdk-lib";
+import { Construct } from "constructs";
+
+export class AwsCdkTutorialStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    new s3.Bucket(this, "MyFirstBucket", {
+      versioned: true,
+    });
+  }
+}
